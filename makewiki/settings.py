922b7cf61c2b2c3f7d150ef124293b51faa5910e
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-12-05 14:59:00 -0800

    [fix] bug

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # wiki app settings
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
